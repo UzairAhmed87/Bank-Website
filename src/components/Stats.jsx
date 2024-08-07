761721969ff3ea8a4e8d455635014af2de15b174
UzairAhmed87 <uzairahmed1574@gmail.com>
2024-08-07 18:46:56 +0500

    complete

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -5,7 +5,7 @@ import {stats} from '../constants/index'
 const Stats = () => (
     <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
 {stats.map((stat) =>(
-  <div key={stat.id}>
+  <div className={`flex-1 flex justify-start items-center flex-row m-3`} key={stat.id}>
 <h4 className='font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px]'>
 {stat.value}
 </h4>
